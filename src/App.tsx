3c8d59f48a9be625543fcb05c14ef462c40deedf
Niranjan Shah <niranjanshah474@gmail.com>
2023-06-02 11:53:23 +1000

    Basic form to upload sentiment created in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Textarea, Button, Heading, Box } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-    Sentiment Analysis Initialized
-      </>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="90vh"
+      flexDirection="column"
+    >
+      <Heading>Welcome to Sentiment Analysis</Heading>
+      <Textarea placeholder='Enter your text here for sentiment analysis' />
+      <Button colorScheme='blue' marginTop="10px" >Predict Sentiment</Button>
+    </Box>
   )
 }
 
